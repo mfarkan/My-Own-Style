ac36d3229aa4c84df9e49d930f13e137476df0a2
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2020-05-27 17:19:41 +0300

    Add project files.

---
+++ src/Web/Security.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Security.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

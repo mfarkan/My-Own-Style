0bd58b13d3951c4acc5ce7f326847029e7f3bc31
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2020-06-23 21:20:11 +0300

    base controller.

---
+++ src/Bridge/HasTextile.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HasTextile.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class BaseController : ControllerBase
+    {
+    }
+}

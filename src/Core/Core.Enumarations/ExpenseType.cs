6a9f54d6ae40d7f648ce18f92d1b49dce5538007
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2020-07-12 14:21:57 +0300

    focusing and starting expenses.

---
+++ src/Core/Core.Enumarations/ExpenseType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Enumarations
+{
+    public enum ExpenseType
+    {
+        OutGoing = 1,
+        InCome = 2
+    }
+}
